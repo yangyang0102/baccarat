d545624e815300a4a01cb80f160313d1676da75b
yangyang0102 <127058361+yangyang0102@users.noreply.github.com>
2026-02-14 15:07:15 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Build v28
-const BUILD_VERSION = "v1.31";
+const BUILD_VERSION = "v1.32";
 
 function onEvent(id, event, handler){
   const el = document.getElementById(id);
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "baccarat-main-only-v1.31";
+const CACHE_NAME = "baccarat-main-only-v1.32";
 const ASSETS = [
   "./",
   "./index.html",
